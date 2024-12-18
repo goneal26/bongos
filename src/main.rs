9d67318a7b81229121f253f0e26cc35b468462f9
aiko <g3oneal@icloud.com>
2024-12-17 23:47:10 -0500

    serial printing and unit test cases

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(bongos::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
+use bongos::println;
 use core::panic::PanicInfo;
 
-mod vga_buffer;
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+  println!("Hello world!");
 
+  #[cfg(test)]
+  test_main();
+
+  loop {} // hang
+}
+
+// called on panic
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
   println!("{}", info);
   loop {}
 }
 
-#[no_mangle]
-pub extern "C" fn _start() -> ! {
-  println!("Hello world!"); // wow!
-
-  loop {} // hang
+// panic handler in test mode
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+  bongos::test_panic_handler(info)
 }
